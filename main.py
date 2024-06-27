a4866b82de4396db0f18c2660e615b34df6ce570
Hugo Lima Farias <hugofarias089@gmail.com>
2024-06-27 05:21:57 -0300

    commit incial

---
+++ main.py
@@ -0,0 +1,14 @@
+# Calculadora de multa de velocidade 
+
+velocidade = float(input('Digite a velocidade do carro em km/h: '))
+
+limite_de_velocidade = 80
+valor_da_multa_km = 20 
+
+if velocidade > limite_de_velocidade:
+    km_excedido = velocidade - limite_de_velocidade
+    valor_multa = km_excedido * valor_da_multa_km
+    print(f'MULTADO! Você ultrapassou o limite de velocidade e levou uma multa de R${valor_multa:.2f}')
+else:
+    print('Tudo certo! Dirija com segurança!')
+
